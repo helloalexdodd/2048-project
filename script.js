9418bba3a0b2f0e0924e9af38efe00cdb68e2b3a
helloalexdodd <hello@alexdodd.ca>
2019-08-15 10:46:56 -0400

    organized code

--- script.js
+++ script.js
@@ -42,17 +42,23 @@ app.isGameOver = () => {
 }
 
 app.isGameWon = () => {
-	app.grid.forEach(row => row.forEach(cell => {
+	app.grid.forEach(col => col.forEach(cell => {
 		if (cell === 2048) { 
 			!confirm(`Winner! Want to keep playing?`) ? window.location.reload() : app.winner = true;
 		} 
 	}))
 }
 
+app.checkForGameEnding = () => {
+	!app.winner ? app.isGameWon() : null
+	app.isGameOver()
+}
+
+// grid operation methods
 app.copyGrid = () => {
 	const newGrid = app.blankGrid();
-	app.grid.forEach((row, i) => {
-		row.forEach((cell, j) => {
+	app.grid.forEach((col, i) => {
+		col.forEach((cell, j) => {
 			newGrid[i][j] = app.grid[i][j]
 		})
 	})
@@ -60,116 +66,58 @@ app.copyGrid = () => {
 }
 
 app.compare = (newGrid) => {
-	newGrid.forEach((row, i) => row.forEach((cell, j) => {
-		if (newGrid[i][j] !== cell) {
-			return true
+	for (let i = 0; i < app.size; i++) {
+		for (let j = 0; j < app.size; j++) {
+			if (newGrid[i][j] !== app.grid[i][j]) {
+				return true
+			}
 		}
-	}))
+	}
 	return false
 }
 
-app.slideRow = (row) => {
-	const numbers = row.filter(value => value)
+app.slideCol = (col) => {
+	const numbers = col.filter(value => value)
 	const zeros = app.size - numbers.length
 	const newZeros = Array(zeros).fill(0)
 	const newArray = newZeros.concat(numbers)
 	return newArray
 }
 
-app.combineRow = (row) => {
+app.combineCol = (col) => {
 	for (let i = 4; i >= 1; i--) {
-		const firstPosition = row[i]
-		const secondPosition = row[i - 1]
+		const firstPosition = col[i]
+		const secondPosition = col[i - 1]
 		if (firstPosition === secondPosition) {
-			row[i] = firstPosition + secondPosition
-			row[i - 1] = 0;
+			col[i] = firstPosition + secondPosition
+			col[i - 1] = 0;
 		}
 	}
-	return row
+	return col
 }
 
-// maybe refactored into new second half of event listener function
-app.operate = (row) => {
-	row = app.slideRow(row)
-	row = app.combineRow(row)
-	row = app.slideRow(row)
-	return row
+app.operate = (col) => {
+	col = app.slideCol(col)
+	col = app.combineCol(col)
+	col = app.slideCol(col)
+	return col
 }
 
-app.rotateGrid = () => {
-	const newGrid = app.blankGrid()
-	app.grid.forEach((row, i) => row.forEach((cell, j) => {
-		if (!app.rotated) {
-			newGrid[i][j] = cell
-			app.rotated = true
-		} else {
-			newGrid[j][i] = cell
-		}
-	}))
-	return newGrid
-}
+app.operateOnGrid = () => {
+	const newGrid = app.copyGrid()
 
-app.flipGrid = () => {
-	app.grid.forEach(row => {
-		row.reverse()
-		app.flipped = true;
+	app.grid.forEach((col, i) => {
+		app.grid[i] = app.operate(col)
 	})
-	return app.grid
+
+	const hasChanged = app.compare(newGrid)
+
+	app.repositionGrid()
+
+	hasChanged ? app.displayNewGrid() : null
 }
 
-app.fillText = () => {
-	app.context.font = `60px Arial`;
-	app.context.fillStyle = `black`;
-	app.context.textAlign = `center`;
-	app.grid.forEach((row, i) => row.forEach((cell, j) => {
-		cell !== 0 ? app.context.fillText(cell, i * 150 + 75, j * 150 + 100) : null
-	}))
-}
-
-app.drawGrid = () => {
-	app.context.beginPath()
-	app.context.lineWidth = 2
-	for (let i = 0; i < app.size; i++) {
-		app.context.moveTo(i * 150, 0)
-		app.context.lineTo(i * 150, 600)	
-	}
-	for (let i = 0; i < app.size; i++) {
-		app.context.moveTo(0, i * 150)
-		app.context.lineTo(600, i * 150)
-	}
-	app.context.stroke()
-}
-
-app.fillGrid = () => {
-	app.grid.forEach((row, i) => row.forEach((cell, j) => {
-		switch (cell) {
-			case 0: app.context.fillStyle = `${app.colours[0]}`; break;
-			case 2: app.context.fillStyle = `${app.colours[1]}`; break;
-			case 4: app.context.fillStyle = `${app.colours[2]}`; break;
-			case 8: app.context.fillStyle = `${app.colours[3]}`; break;
-			case 16: app.context.fillStyle = `${app.colours[4]}`; break;
-			case 32: app.context.fillStyle = `${app.colours[5]}`; break;
-			case 64: app.context.fillStyle = `${app.colours[6]}`; break;
-			case 128: app.context.fillStyle = `${app.colours[7]}`; break;
-			case 256: app.context.fillStyle = `${app.colours[8]}`; break;
-			case 512: app.context.fillStyle = `${app.colours[9]}`; break;
-			case 1024: app.context.fillStyle = `${app.colours[10]}`; break;
-			case 2048: app.context.fillStyle = `${app.colours[11]}`; break;
-			case 4096: app.context.fillStyle = `${app.colours[12]}`; break;
-			case 8192: app.context.fillStyle = `${app.colours[13]}`; break;
-			case 16384: app.context.fillStyle = `${app.colours[14]}`; break;
-		}
-		app.context.fillRect(i * 150, j * 150, 150, 150)
-	}))
-}
-
-app.displayNewGrid = () => {	
-	app.clearBoard()
-	app.drawBoard()
-	!app.winner ? app.isGameWon() : null
-	app.isGameOver()
-}
-
+// board positioning methods
 app.repositionGrid = () => {
 	if (app.flipped) {
 		app.grid = app.flipGrid()
@@ -182,19 +130,25 @@ app.repositionGrid = () => {
 	}
 }
 
-app.operateOnGrid = () => {
-	// the order of these lines super matters
-	const newGrid = app.copyGrid()
-
-	app.grid.forEach((row, i) => {
-		app.grid[i] = app.operate(row)
+app.flipGrid = () => {
+	app.grid.forEach(col => {
+		col.reverse()
+		app.flipped = true;
 	})
+	return app.grid
+}
 
-	const hasChanged = app.compare(newGrid)
-
-	app.repositionGrid()
-
-	hasChanged ? app.displayNewGrid() : null
+app.rotateGrid = () => {
+	const newGrid = app.blankGrid()
+	app.grid.forEach((col, i) => col.forEach((cell, j) => {
+		if (!app.rotated) {
+			newGrid[i][j] = cell
+			app.rotated = true
+		} else {
+			newGrid[j][i] = cell
+		}
+	}))
+	return newGrid
 }
 
 app.positionGrid = () => {
@@ -224,11 +178,12 @@ app.positionGrid = () => {
 	}
 }
 
+// add new number method
 app.addNumber = () => {
 	const zeroLocations = [];
-	app.grid.map((row, i) => {
-		return row.reduce((acc, cur, j) => {
-			row[j] === 0 ? acc.push({ x: i, y: j }) : null
+	app.grid.map((col, i) => {
+		return col.reduce((acc, cur, j) => {
+			col[j] === 0 ? acc.push({ x: i, y: j }) : null
 			return acc
 		}, zeroLocations)
 	})
@@ -239,6 +194,57 @@ app.addNumber = () => {
 	}
 }
 
+// board drawing methods
+app.fillGrid = () => {
+	app.grid.forEach((col, i) => col.forEach((cell, j) => {
+		switch (cell) {
+			case 0: app.context.fillStyle = `${app.colours[0]}`; break;
+			case 2: app.context.fillStyle = `${app.colours[1]}`; break;
+			case 4: app.context.fillStyle = `${app.colours[2]}`; break;
+			case 8: app.context.fillStyle = `${app.colours[3]}`; break;
+			case 16: app.context.fillStyle = `${app.colours[4]}`; break;
+			case 32: app.context.fillStyle = `${app.colours[5]}`; break;
+			case 64: app.context.fillStyle = `${app.colours[6]}`; break;
+			case 128: app.context.fillStyle = `${app.colours[7]}`; break;
+			case 256: app.context.fillStyle = `${app.colours[8]}`; break;
+			case 512: app.context.fillStyle = `${app.colours[9]}`; break;
+			case 1024: app.context.fillStyle = `${app.colours[10]}`; break;
+			case 2048: app.context.fillStyle = `${app.colours[11]}`; break;
+			case 4096: app.context.fillStyle = `${app.colours[12]}`; break;
+			case 8192: app.context.fillStyle = `${app.colours[13]}`; break;
+			case 16384: app.context.fillStyle = `${app.colours[14]}`; break;
+		}
+		app.context.fillRect(i * 150, j * 150, 150, 150)
+	}))
+}
+
+app.drawGrid = () => {
+	app.context.beginPath()
+	app.context.lineWidth = 2
+	for (let i = 0; i < app.size; i++) {
+		app.context.moveTo(i * 150, 0)
+		app.context.lineTo(i * 150, 600)
+	}
+	for (let i = 0; i < app.size; i++) {
+		app.context.moveTo(0, i * 150)
+		app.context.lineTo(600, i * 150)
+	}
+	app.context.stroke()
+}
+
+app.fillText = () => {
+	app.context.font = `60px Arial`;
+	app.context.fillStyle = `black`;
+	app.context.textAlign = `center`;
+	app.grid.forEach((col, i) => col.forEach((cell, j) => {
+		cell !== 0 ? app.context.fillText(cell, i * 150 + 75, j * 150 + 100) : null
+	}))
+}
+
+app.clearBoard = () => {
+	app.context.clearRect(0, 0, 800, 800);
+}
+
 app.drawBoard = () => {
 	app.addNumber()
 	app.fillGrid()
@@ -246,10 +252,13 @@ app.drawBoard = () => {
 	app.fillText()
 }
 
-app.clearBoard = () => {
-	app.context.clearRect(0, 0, 800, 800);
+app.displayNewGrid = () => {
+	app.clearBoard()
+	app.drawBoard()
+	app.checkForGameEnding()
 }
 
+// initial page loaded methods
 app.init = () => {
 	app.grid = app.blankGrid()
 	app.addNumber()

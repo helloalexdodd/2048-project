fb919646b0018569658a523f1ee2e674a23adf2a
helloalexdodd <hello@alexdodd.ca>
2019-08-23 16:21:07 -0400

    added commonjs

--- script.js
+++ script.js
@@ -219,7 +219,10 @@ app.detectSwipe = () => {
 	let direction = 0
 	const el = document.getElementById(`body`)
 
-	bodyScrollLock.disableBodyScroll(el)
+	const bodyScrollLock = require('body-scroll-lock');
+	const disableBodyScroll = bodyScrollLock.disableBodyScroll
+		
+	disableBodyScroll(el)
  
 	el.addEventListener(`touchstart`, (e) => {
 		const t = e.touches[0]
